1d50a1e8c4e79d58025b7ebcb1410eabfbf6538d
jsteel <joesteel2010@gmail.com>
2018-02-09 12:41:50 +0000

    Exported Api type

--- api.go
+++ api.go
@@ -1,4 +1,4 @@
-package model
+package main
 
 import (
 	"net/http"
@@ -7,21 +7,31 @@ import (
 	"io"
 	"bytes"
 	"encoding/xml"
+	"strconv"
+)
+
+const (
+	HeaderAuthorizationKey      = "Authorization"
+	HeaderIfModifiedSinceKey    = "If-Modified-Since"
+	HeaderTokenKey              = "Token"
+	HeaderTokenAuthFormatString = "Basic %s"
 )
 
 type TokenStorage interface {
 	Save(token token) error
-	Load() (token, error)
+	Load() (*token, error)
 }
 
-type api struct {
-	dataFeedId  string
-	credentials *credentials
+type Api struct {
+	dataFeedId   string
+	credentials  *credentials
 	tokenStorage TokenStorage
+	Error        error
+	StatusCode   int
 }
 
-func Api(dataFeedId string, username string, password string) (api *api) {
-	return &api{
+func NewApi(dataFeedId string, username string, password string) (*Api) {
+	return &Api{
 		dataFeedId: dataFeedId,
 		credentials: &credentials{
 			userName: username,
@@ -30,12 +40,12 @@ func Api(dataFeedId string, username string, password string) (api *api) {
 	}
 }
 
-func (api *api) SetTokenStorage(tokenStorage TokenStorage) {
+func (api *Api) SetTokenStorage(tokenStorage TokenStorage) {
 	api.tokenStorage = tokenStorage
 }
 
 // GetBranches returns an array of branches
-func (api *api) GetBranches() (*BranchSummaries, error) {
+func (api *Api) GetBranches() (*BranchSummaries, error) {
 	branches := new(BranchSummaries)
 	branchesURLBuilder := new(URLGetBranchesBuilder)
 	branchesURLBuilder.SetDataFeedID(api.dataFeedId)
@@ -45,50 +55,53 @@ func (api *api) GetBranches() (*BranchSummaries, error) {
 	return branches, nil
 }
 
-func (api *api) GetBranch(branchSummary *BranchSummary) (branch *Branch, err error) {
-	var clientID int
-	if clientID, err = branchSummary.GetClientID(); err != nil {
-		return nil, err
-	}
+func (api *Api) GetBranch(branchSummary *BranchSummary) (branch *Branch, err error) {
+	branch = new(Branch)
 	branchURLBuilder := new(URLGetBranchBuilder)
 	branchURLBuilder.SetDataFeedID(api.dataFeedId)
-	branchURLBuilder.SetClientID(string(clientID))
+	branchURLBuilder.SetClientID(branchSummary.GetClientIDString())
 	if err := api.doRequest(branchURLBuilder, branch); err != nil {
 		return nil, err
 	}
 	return branch, nil
 }
 
-func (api *api) GetProperties(branchSummary *BranchSummary) (properties *PropertySummaries, err error) {
-	var clientID int
-	if clientID, err = branchSummary.GetClientID(); err != nil {
-		return nil, err
-	}
+func (api *Api) GetProperties(branchSummary *BranchSummary) (properties *PropertySummaries, err error) {
+	properties = new(PropertySummaries)
 	propertiesURLBuilder := new(URLGetPropertiesBuilder)
 	propertiesURLBuilder.SetDataFeedID(api.dataFeedId)
-	propertiesURLBuilder.SetClientID(string(clientID))
+	propertiesURLBuilder.SetClientID(branchSummary.GetClientIDString())
 	if err := api.doRequest(propertiesURLBuilder, properties); err != nil {
 		return nil, err
 	}
 	return properties, nil
 }
 
-func (api *api) GetProperty(branchSummary *BranchSummary, summary PropertySummary) (property *Property, err error) {
-	var clientID int
-	if clientID, err = branchSummary.GetClientID(); err != nil {
-		return nil, err
-	}
+func (api *Api) GetProperty(branchSummary *BranchSummary, summary PropertySummary) (property *Property, err error) {
+	property = new(Property)
 	propertyURLBuilder := new(URLGetPropertyBuilder)
 	propertyURLBuilder.SetDataFeedID(api.dataFeedId)
-	propertyURLBuilder.SetClientID(string(clientID))
-	propertyURLBuilder.SetPropertyID(string(summary.PropertyID))
+	propertyURLBuilder.SetClientID(branchSummary.GetClientIDString())
+	propertyURLBuilder.SetPropertyID(strconv.Itoa(int(summary.PropertyID)))
 	if err := api.doRequest(propertyURLBuilder, property); err != nil {
 		return nil, err
 	}
 	return property, nil
 }
 
-func (api *api) GetChangedProperties(since time.Time) (properties *PropertySummaries, err error) {
+
+func (api *Api) GetPropertyFromChangedFileSummary(summary ChangedFileSummary) (property *Property, err error) {
+	property = new(Property)
+	propertyURLBuilder := new(ChangedPropertyURLBuilder)
+	propertyURLBuilder.SetURL(summary.PropUrl)
+	if err := api.doRequest(propertyURLBuilder, property); err != nil {
+		return nil, err
+	}
+	return property, nil
+}
+
+func (api *Api) GetChangedProperties(since time.Time) (properties *ChangedPropertySummaries, err error) {
+	properties = new(ChangedPropertySummaries)
 	propertiesURLBuilder := new(URLGetChangedPropertiesBuilder)
 	propertiesURLBuilder.SetDataFeedID(api.dataFeedId)
 	propertiesURLBuilder.SetSince(since)
@@ -98,7 +111,21 @@ func (api *api) GetChangedProperties(since time.Time) (properties *PropertySumma
 	return properties, nil
 }
 
-func (api *api) GetChangedFiles(since time.Time) (changedFiles *ChangedFilesSummaries, err error) {
+func (api *Api) GetChangedProperty(changedProperty *ChangedPropertySummary) (property *Property, err error) {
+	if changedProperty.LastAction == Deleted {
+		return nil, fmt.Errorf("property [%s] has been deleted", strconv.Itoa(int(changedProperty.PropertyID)))
+	}
+	property = new(Property)
+	propertiesURLBuilder := new(ChangedPropertyURLBuilder)
+	propertiesURLBuilder.SetURL(changedProperty.Url)
+	if err = api.doRequest(propertiesURLBuilder, property); err != nil {
+		return nil, err
+	}
+	return property, nil
+}
+
+func (api *Api) GetChangedFiles(since time.Time) (changedFiles *ChangedFilesSummaries, err error) {
+	changedFiles = new(ChangedFilesSummaries)
 	urlGetChangedFilesBuilder := new(URLGetChangedFilesBuilder)
 	urlGetChangedFilesBuilder.SetDataFeedID(api.dataFeedId)
 	urlGetChangedFilesBuilder.SetSince(since)
@@ -108,12 +135,16 @@ func (api *api) GetChangedFiles(since time.Time) (changedFiles *ChangedFilesSumm
 	return changedFiles, nil
 }
 
-func (api *api)doRequest(urlBuilder URLBuilder, out interface{}) error {
+func (api *Api) doRequest(urlBuilder URLBuilder, out interface{}) (err error) {
 	requestor := buildRequestor(api.dataFeedId, api.credentials)
+	if api.tokenStorage != nil {
+		if requestor.token, err = api.tokenStorage.Load(); err != nil {
+			return err
+		}
+	}
 	requestor.urlBuilder = urlBuilder
-	for ;requestor.attempts < 2;requestor.attempts++ {
+	for ; requestor.attempts < 2; requestor.attempts++ {
 		requestor.buildRequest()
-		requestor.setAuthenticationMethod()
 		requestor.doRequest()
 		requestor.saveTokenIfExists(api.tokenStorage)
 		requestor.handleErrors()
@@ -121,7 +152,32 @@ func (api *api)doRequest(urlBuilder URLBuilder, out interface{}) error {
 			return requestor.unmarshal(out)
 		}
 	}
-	return fmt.Errorf(requestor.response.Status)
+	api.StatusCode = requestor.response.StatusCode
+	api.Error = fmt.Errorf(requestor.response.Status)
+	return api.Error
+}
+
+func (api *Api) doRequestSince(urlBuilder URLBuilder, out interface{}, since *time.Time) (err error) {
+	requestor := buildRequestor(api.dataFeedId, api.credentials)
+	requestor.setIfModifiedSince(since)
+	if api.tokenStorage != nil {
+		if requestor.token, err = api.tokenStorage.Load(); err != nil {
+			return err
+		}
+	}
+	requestor.urlBuilder = urlBuilder
+	for ; requestor.attempts < 2; requestor.attempts++ {
+		requestor.buildRequest()
+		requestor.doRequest()
+		requestor.saveTokenIfExists(api.tokenStorage)
+		requestor.handleErrors()
+		api.StatusCode = requestor.response.StatusCode
+		if requestor.response.StatusCode == http.StatusOK {
+			return requestor.unmarshal(out)
+		}
+	}
+	api.Error = fmt.Errorf(requestor.response.Status)
+	return api.Error
 }
 
 type credentials struct {
@@ -178,12 +234,31 @@ func buildRequestor(dataFeedId string, credentials *credentials) (*requestor) {
 }
 
 func (requestor *requestor) doRequest() {
-	requestor.response, requestor.err = http.DefaultClient.Do(requestor.request)
+	requestor.response, requestor.err = (&http.Client{}).Do(requestor.request)
+	if requestor.err != nil {
+		panic(requestor.err)
+	}
+}
+
+func (requestor *requestor) saveTokenIfExists(tokenStorage TokenStorage) {
+	if token := requestor.response.Header.Get(HeaderTokenKey); token != "" {
+		requestor.token = Token(token)
+		if tokenStorage != nil {
+			tokenStorage.Save(*requestor.token)
+		}
+	}
+}
+func (requestor *requestor) handleErrors() {
+	switch requestor.response.StatusCode {
+	case http.StatusUnauthorized:
+		requestor.token.Invalidate()
+	default:
+	}
 }
 
 func (requestor *requestor) setIfModifiedSince(since *time.Time) {
 	if since != nil {
-		requestor.header.Add("If-Modified-Since", since.Format(time.RFC1123))
+		requestor.header.Add(HeaderIfModifiedSinceKey, since.Format(time.RFC1123))
 	}
 }
 
@@ -196,21 +271,15 @@ func (requestor *requestor) setBasicAuth() {
 }
 
 func (requestor *requestor) setAuthenticationToken() {
-	requestor.header.Add("Authorization", fmt.Sprintf("Basic %s", string(requestor.token.tokenString)))
-}
-
-func (requestor *requestor) saveTokenIfExists(tokenStorage TokenStorage) {
-	if token := requestor.response.Header.Get("Token"); token != "" {
-		requestor.token = Token(token)
-		if tokenStorage != nil {
-			tokenStorage.Save(*requestor.token)
-		}
-	}
+	requestor.request.Header.Add(HeaderAuthorizationKey, fmt.Sprintf(HeaderTokenAuthFormatString, requestor.token.tokenString))
 }
 
 func (requestor *requestor) buildRequest() (err error) {
 	requestor.request, err = http.NewRequest(http.MethodGet, requestor.urlBuilder.Build(), requestor.body)
-	requestor.request.Header = requestor.header
+	for key, val := range requestor.header {
+		requestor.request.Header[key] = val
+	}
+	requestor.setAuthenticationMethod()
 	return err
 }
 
@@ -230,12 +299,4 @@ func (requestor *requestor) unmarshal(out interface{}) error {
 	}
 	err := xml.Unmarshal(bodyBuffer.Bytes(), out)
 	return err
-}
-
-func (requestor *requestor) handleErrors() {
-	switch requestor.response.StatusCode {
-	case http.StatusUnauthorized:
-		requestor.token.Invalidate()
-	default:
-	}
 }
